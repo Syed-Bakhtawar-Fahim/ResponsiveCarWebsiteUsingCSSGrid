7d37ff49af32b627c1cbc23605c8fc3709163d1c
Syed Bakhtawar Fahim <87495620+Syed-Bakhtawar-Fahim@users.noreply.github.com>
2021-08-15 18:47:40 -0700

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,15 @@
+
+// let opt1_Animate = document.getElementsByClassName('opt1')
+const showContent=()=>{
+    const para=document.getElementById('rent-h4').style.display="block"
+}
+const hideContent=()=>{
+    const para=document.getElementById('rent-h4').style.display="none"
+}
+
+const showContent1=()=>{
+    const para=document.getElementsByClassName('hide').style.display="block"
+}
+const hideContent1=()=>{
+    const para=document.getElementsByClassName('hide').style.display="none"
+}
